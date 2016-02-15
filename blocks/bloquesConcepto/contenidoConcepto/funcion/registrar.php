dcbcb4299038d9cdf17d0438b8f0db5c4207ee51
Juan David Lara <jdavid.6700@gmail.com>
2016-02-15 14:18:08 -0500

    Commit Logica Conceptos Preliminar Analizador Sintactico Primera version de la integración

--- blocks/bloquesConcepto/contenidoConcepto/funcion/registrar.php
+++ blocks/bloquesConcepto/contenidoConcepto/funcion/registrar.php
@@ -4,6 +4,7 @@ namespace bloquesConcepto\contenidoConcepto\funcion;
 
 
 include_once('Redireccionador.php');
+include_once('Interprete.php');
 
 class FormProcessor {
     
@@ -41,7 +42,8 @@ class FormProcessor {
         $_entradaFormulaCompilador = $_REQUEST['formulaConcepto'];
         
         
-        
+        var_dump(Interprete::evaluarSentencia($_entradaFormulaCompilador));
+        exit;
         
         
         
